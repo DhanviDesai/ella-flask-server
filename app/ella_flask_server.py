709dd8694977d8d6602cc637a73c4948d68c5c4a
DhanviDesai <dhanvidesai99@gmail.com>
2020-07-02 22:28:25 +0530

    API test 28, Gunicorn timeout,Download_link,Paper_details-7

--- app/ella_flask_server.py
+++ app/ella_flask_server.py
@@ -93,11 +93,9 @@ def extract_papers(web_driver,query,rows_per_page):
             refinement_content_type.append(div.text)
         final_paper_list["refinement_content_type"] = refinement_content_type
         papers_range = []
-        outer_span = driver.find_elements_by_class_name("span-horizantal")[0]
-        inner_span = outer_span.find_elements_by_tag_name("span")[0]
-        inner_span_labels = inner_span.find_elements_by_tag_name("label")
-        papers_range.append(inner_span_labels[0].text)
-        papers_range.append(inner_span_labels[1].text)
+        range_divs = driver.find_elements_by_class_name("noUi-tooltip")
+        papers_range.append(range_divs[0].text)
+        papers_range.append(range_divs[1].text)
         final_paper_list["papers_range"] = papers_range
         results = driver.find_elements_by_class_name("List-results-items")
         final_results = []
