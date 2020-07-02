22f6f090e23126d87621c9c7ea16a0e34884f6cf
DhanviDesai <dhanvidesai99@gmail.com>
2020-07-02 13:11:11 +0530

    API test 15

--- app/ella_flask_server.py
+++ app/ella_flask_server.py
@@ -8,12 +8,15 @@ import sys
 import math
 import time
 import os
+import json
 
 base_url = "https://ieeexplore.ieee.org/search/searchresult.jsp?"
 
 GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google_chrome'
 
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+web_driver = None
 
 app = Flask(__name__)
 
@@ -31,9 +34,10 @@ def initialize_webdriver():
     print('Finished initalize')
     return webdriver1
 
-def get_paper_title(div):
+def get_paper_title_and_link(div):
     h2 = div.find_elements_by_tag_name("h2")[0]
-    return h2.text
+    paper_link = h2.find_elements_by_tag_name("a")[0].get_attribute("href")
+    return h2.text,paper_link
 
 def get_paper_authors(div):
     authors = []
@@ -61,36 +65,55 @@ def get_total_count_and_pages(div):
     span = dashboard_header.find_elements_by_tag_name("span")[0]
     strong_span = span.find_elements_by_tag_name("span")[1]
     total_count = int(strong_span.text.replace(",",""))
-    total_pages = math.ceil(total_count/25)
+    total_pages = math.ceil(total_count/10)
     return total_count,total_pages
 
 def extract_papers(query):
     start = time.time()
     query_url = attach_search_query(query)
-    webdriver = initialize_webdriver()
-    with webdriver as driver:
-        wait = WebDriverWait(driver,8)
+    if web_driver == None:
+        web_driver = initialize_webdriver()
+    final_paper_list = []
+    with web_driver as driver:
+        wait = WebDriverWait(driver,20)
         driver.get(query_url)
         wait.until(presence_of_element_located((By.CLASS_NAME,"main-section")))
+        metadata = {}
         print('Finished waiting')
         total_count,total_pages = get_total_count_and_pages(driver.find_elements_by_class_name("Dashboard-section")[0])
-        print(total_count)
-        print(total_pages)
-        print("*********************************************************************")
+        metadata["total_count"] = total_count
+        metadata["total_pages"] = total_pages
+        #print(total_count)
+        #print(total_pages)
+        #print("*********************************************************************")
+        final_paper_list.append(metadata)
         results = driver.find_elements_by_class_name("List-results-items")
         for div in results:
-            title = get_paper_title(div)
+            paper = {}
+            title,paper_link = get_paper_title_and_link(div)
             authors = get_paper_authors(div)
             description,publisher_info = get_paper_description(div)
-            print(title)
-            print(authors)
-            print(description)
-            print(publisher_info)
-            print('--------------------------------------------------------------------------------------------------------')
-        driver.close()
+            paper["title"] = title
+            paper["authors"] = authors
+            paper["description"] = description
+            paper["publisher_info"] = publisher_info
+            paper["paper_link"] = paper_link
+            #print(title)
+            #print(authors)
+            #print(description)
+            #print(publisher_info)
+            #print('--------------------------------------------------------------------------------------------------------')
+            final_paper_list.append(paper)
+        #driver.close()
     end = time.time()
     print(end-start)
+    return json.dumps(final_paper_list)
 
 @app.route('/')
-def main_root():
-    extract_papers('ml')
+def home_page():
+    return "<h1>Welcome to Ella Server</h1>"
+
+@app.route("/search",methods=["GET","POST"])
+def search():
+    query_text = request.args.get("queryText")
+    extract_papers(query_text)
