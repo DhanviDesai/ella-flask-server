72c669f5e3beb2625dae159712a466648e018f7c
DhanviDesai <dhanvidesai99@gmail.com>
2020-07-03 13:37:12 +0530

    API test 28, Gunicorn timeout,Download_link,Paper_details-12,Split into modules-3

--- app/ella_flask_server.py
+++ app/ella_flask_server.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask import request
 import os
 
-#from os.path.join(os.getcwd(),"/extract_ieee_papers") import extract_papers,get_paper_link_details,get_download_link
+from .extract_ieee_papers import extract_papers,get_paper_link_details,get_download_link,initialize_webdriver
 #import os.path.normpath(os.path.join(os.getcwd(),"/extract_ieee_papers"))
-import extract_ieee_papers
+#import extract_ieee_papers
 
 app = Flask(__name__)
 
@@ -22,8 +22,8 @@ def search():
     range = request.args.get("range")
     filters = request.args.get("filters")
     print(filters)
-    web_driver = extract_ieee_papers.initialize_webdriver()
-    response = extract_ieee_papers.extract_papers(web_driver,query_text,rows_per_page,range,filters)
+    web_driver = initialize_webdriver()
+    response = extract_papers(web_driver,query_text,rows_per_page,range,filters)
     return response
 
 @app.route("/paperLink",methods=["GET","POST"])
@@ -31,8 +31,8 @@ def paper_link():
     start = time.time()
     link = request.args.get("link")
     type = request.args.get("type")
-    web_driver = extract_ieee_papers.initialize_webdriver()
-    response = extract_ieee_papers.get_paper_link_details(web_driver,link,type)
+    web_driver = initialize_webdriver()
+    response = get_paper_link_details(web_driver,link,type)
     end = time.time()
     print(end-start)
     return response
@@ -41,7 +41,7 @@ def paper_link():
 def download_link_endpoint():
     start = time.time()
     link = request.args.get("link")
-    status,download_link = extract_ieee_papers.get_download_link(link)
+    status,download_link = get_download_link(link)
     response = {}
     response["status"] = status
     response["download_link"] = download_link
